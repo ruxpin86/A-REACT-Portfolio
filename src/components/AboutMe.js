125eadb3f40db63c740e241c91b30f5cd3041ed7
William Glynn <teddy_gram@Williams-Mac-mini.local>
2022-07-14 10:38:49 -0600

    contact form work

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,36 @@
 import React from "react";
+import Image from "./images/thumbnail.jpeg";
 
 export default function AboutMe() {
-  return <div>AboutMe</div>;
+  return (
+    <>
+      <br></br>
+      <main>
+        <h2>Ted Glynn</h2>
+        <section>
+          <img src={Image} />
+          <div>
+            <h4 className="smooth">"Smooth Brain Boy"</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+              quae saepe, itaque fugiat, consequatur voluptas, sint iste maxime
+              eligendi ut placeat libero officiis tempore nostrum numquam quod
+              quo mollitia officia?Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque quae saepe, itaque fugiat, consequatur
+              voluptas, sint iste maxime eligendi ut placeat libero officiis
+              tempore nostrum numquam quod quo mollitia officia?Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Neque quae saepe,
+              itaque fugiat, consequatur voluptas, sint iste maxime eligendi ut
+              placeat libero officiis tempore nostrum numquam quod quo mollitia
+              officia?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque quae saepe, itaque fugiat, consequatur voluptas, sint iste
+              maxime eligendi ut placeat libero officiis tempore nostrum numquam
+              quod quo mollitia officia?
+            </p>
+          </div>
+          <div></div>
+        </section>
+      </main>
+    </>
+  );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <main>
+        <h2>Hit me up!</h2>
+        <form>
+          <label for="name">Name</label>
+          <input type="text" id="name"></input>
+          <label for="name">Email</label>
+          <input type="text" id="name"></input>
+          <label for="name">Message</label>
+          <textarea type="textarea" id="name"></textarea>
+          <button type="button">Submit</button>
+        </form>
+      </main>
+    </>
+  );
 }
