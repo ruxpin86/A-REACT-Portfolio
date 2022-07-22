dc15fc7f7600d848b722d8264974bc11c4e1ec8c
William Glynn <teddy_gram@Williams-Mac-mini.local>
2022-07-21 19:26:39 -0600

    adding to Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,28 @@ export default function Resume() {
       <br></br>
       <main>
         <h3 className="resume">Skills & Resume</h3>
-        <section></section>
+        <section>
+          <h4>Front-End Proficiencies</h4>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Responsive Design</li>
+          </ul>
+          <h4>Back-End Proficiencies</h4>
+          <ul>
+            <li>Node</li>
+            <li>MySql</li>
+            <li>Sequelize</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Mongooose</li>
+            <li>REST</li>
+            <li>APIs</li>
+          </ul>
+        </section>
       </main>
     </>
   );
